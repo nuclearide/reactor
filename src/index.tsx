503d7bec27be17af507203a27abced9144484fb1
Simon Hochrein <simon@ministrycrm.com>
2018-02-26 14:28:06 -0600

    Add build system

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export default class Editor extends React.Component {
+    render() {
+        return (
+            <h1>test</h1>
+        );
+    }
+}
---
+++ test/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import Editor from '../src/index';
+import {render} from 'react-dom';
+
+render(<Editor/>, document.getElementById('root'));
